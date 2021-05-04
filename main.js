a5033970fd9fa0211727ee37e7a9278ec1868e61
Darparish <hosseindarp@gmail.com>
2021-05-04 15:50:50 -0400

    finish homepage & add sass

--- main.js
+++ main.js
@@ -7,9 +7,7 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
+    webPreferences: {},
   });
 
   // and load the index.html of the app.
